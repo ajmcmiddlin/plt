32d13e3312b2a436a7e893c9b8a127cb295fc2ec
Andrew McMiddlin <andrew@ajmccluskey.com>
2019-03-06 16:51:29 +1000

    Start Hutton's Razor.

---
+++ HuttonsRazor.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE LambdaCase #-}
+-- | Solution for https://www.codewars.com/kata/huttons-razor
+
+data Razor =
+  Lit Int
+  | Add Razor Razor
+
+ex1 :: Razor
+ex1 = Add (Lit 1) (Lit 2)
+
+ex2 :: Razor
+ex2 = Add (Lit 1) (Add (Lit 2) (Add (Lit 3) (Lit 4)))
+
+interpret ::
+  Razor
+  -> Int
+interpret = \case
+  Lit n -> n
+  Add r1 r2 -> interpret r1 + interpret r2
+
+pretty ::
+  Razor
+  -> String
+pretty = \case
+  Lit n -> show n
+  Add r1 r2 -> "(" <> pretty r1 <> "+" <> pretty r2 <> ")"
