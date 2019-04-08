f94ef48e804e84b0ec174875b3ef7247f4db631d
Andrew McCluskey <andrew@ajmccluskey.com>
2019-04-08 16:42:26 +1000

    Knock out checking/inference.

--- huttons-razor/lib/Types.hs
+++ huttons-razor/lib/Types.hs
@@ -5,6 +5,7 @@
 module Types where
 
 import           Control.Lens             (makeClassyPrisms)
+import           Control.Monad            (void)
 import           Control.Monad.Error.Lens (throwing)
 import           Control.Monad.Except     (MonadError)
 import           Data.Bool                (bool)
@@ -39,6 +40,52 @@ infer = \case
     binOp op r1 r2 = do
       ty1 <- infer r1
       ty2 <- infer r2
-      bool (mismatch op ty1 ty2) (pure ty1) $ ty1 == ty2
-    mismatch op ty1 ty2 =
-      throwing _TypeError $ MismatchedTypes op ty1 ty2
+      bool (terror $ MismatchedTypes op ty1 ty2) (pure ty1) $ ty1 == ty2
+
+check ::
+  ( AsTypeError e
+  , MonadError e m
+  )
+  => Type
+  -> Razor
+  -> m ()
+check tyExpected = \case
+  (LitI _) -> checkLit "integer" TyInteger
+  (LitB _) -> checkLit "boolean" TyBool
+  (Add r1 r2) ->
+    void . sequence $ [
+      check TyInteger r1
+    , check TyInteger r2
+    , check' UnexpectedType "+" tyExpected TyInteger
+    ]
+  (Or r1 r2) ->
+    void . sequence $ [
+      check TyBool r1
+    , check TyBool r2
+    , check' UnexpectedType "||" tyExpected TyBool
+    ]
+  (IfThenElse b t f) -> do
+    tyT <- infer t
+    tyF <- infer f
+    void . sequence $ [
+        check TyBool b
+      , check' MismatchedTypes "if" tyT tyF
+      , check' UnexpectedType "if" tyExpected tyT
+      ]
+  where
+    check' f desc tyExpected' ty =
+      bool
+        (terror $ f desc tyExpected' ty)
+        (pure ())
+        (tyExpected == ty)
+    checkLit name =
+      check' UnexpectedType (name <> " literal") tyExpected
+
+terror ::
+  ( AsTypeError e
+  , MonadError e m
+  )
+  => TypeError
+  -> m a
+terror =
+  throwing _TypeError
